16b726ddfd1cc3fcd14bd26e079fbcceb631f7ce
Nevin <nevinthomas48@gmail.com>
2023-06-03 20:15:37 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 
 import SignInForm from './Components/SignInForm';
+import Navbar from './Components/NavBar';
 
 function App() {
   return (
+    <div>
+    <Navbar />
 
     <SignInForm />
+    </div>
+   
 
 
 
